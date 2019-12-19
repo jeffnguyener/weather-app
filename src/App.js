6ed8a0a78b41206120710dc124cd6b522adfead5
jeffnguyener <jeffnguyen.83@gmail.com>
2019-12-18 21:54:51 -0600

    API key setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import Weather from './components/Weather'
-import './App.css';
+import React from "react";
+import Weather from "./components/Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
+import "weather-icons/css/weather-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+//API CALL - api.openweathermap.org/data/2.5/weather?q=London,uk
+const API_KEY = "82c474b0047181f823e85c3e413f2346";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk`)
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
