7f3e0ab933b7407ac91f849ba75fcdac978d78da
jeffnguyener <jeffnguyen.83@gmail.com>
2019-12-19 09:53:13 -0600

    Added celsius to fahrenheit conv & rendered props

--- src/App.js
+++ src/App.js
@@ -11,18 +11,56 @@ const API_KEY = "82c474b0047181f823e85c3e413f2346";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      city: "",
+      country: "",
+      icon: "",
+      fahrenheit: "",
+      temp_max: "",
+      temp_min: "",
+      description: "",
+      error: false
+    };
+    this.getWeather();
+  }
+
+  calFahrenheit(temp) {
+    let cel = Math.floor(temp - 273.15);
+    let fahr = (cel * 9) / 5 + 32;
+    return fahr;
   }
 
   getWeather = async () => {
-    const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk`)
-    
-  }
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`
+    );
+
+    const response = await api_call.json();
+
+    console.log(response);
+
+    this.setState({
+      city: response.name,
+      country: response.sys.country,
+      icon: response.weather.icon,
+      fahrenheit: this.calFahrenheit(response.main.temp),
+      temp_max: response.main.temp_max,
+      temp_min: response.main.temp_min,
+      description: response.weather.description
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <Weather />
+        <Weather
+          city={this.state.city}
+          country={this.state.country}
+          temp_fahrenheit={this.state.fahrenheit}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
+          description={this.state.description}
+        />
       </div>
     );
   }
